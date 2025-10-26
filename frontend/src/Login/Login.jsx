9b01b87006dab2125d754cc66893ae3fd546d377
Vijeta Kumari <vijeta@example.com>
2025-10-26 22:45:41 +0530

    Added Login & Signup

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -1,7 +1,36 @@
 import "./Login.css";
 
 function Login() {
-  return <></>;
+  return <>
+     {/*  Main container for the login form  */}
+    <div class="container">
+      {/*quiz form Heading*/}
+      
+      <h2>Login</h2>
+     
+
+        {/*  Login form starts  */}
+        <form action="#">
+            {/* Username input field  */}
+            <div class="input-box">
+                <label for="username">Username</label>
+                <input type="text" id="username" placeholder="Enter your username" required />
+            </div>
+
+            {/*  Password input field  */}
+            <div class="input-box">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Enter your password" required />
+            </div>
+
+            {/*  Login button  */}
+            <button type="submit" class="btn">Login</button>
+
+            {/*  Link to sign-up page  */}
+            <p class="link">Don’t have an account? <a href="signup.html">Sign up</a></p>
+        </form>
+    </div>
+  </>;
 }
 
 export default Login;
